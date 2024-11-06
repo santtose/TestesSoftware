74c8b4a134bdf9d954bfbe977288d80c2c33e325
santtose <santtose@outlook.com>
2024-11-06 16:05:00 -0300

    iniciando testes pedido

---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,20 @@
+﻿namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        public decimal ValorTotal { get; set; }
+        private readonly List<PedidoItem> _pedidoItems;
+        private IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+     
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            _pedidoItems.Add(pedidoItem);
+            ValorTotal = PedidoItems.Sum(i => i.Quantidade * i.ValorUnitario);
+        }
+    }
+}
---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,21 @@
+﻿namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar Item Pedido Vazio")]
+        [Trait("Categoria", "Pedido Testes")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arrange
+            //var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            // Act
+            pedido.AdicionarItem(pedidoItem);
+
+            // Assert
+            Assert.Equal(200, pedido.ValorTotal);
+        }
+    }
+}
