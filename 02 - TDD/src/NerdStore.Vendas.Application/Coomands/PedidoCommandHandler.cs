a555e572f66768553f82fc5572e559353f3c94e6
santtose <santtose@outlook.com>
2024-11-14 11:30:35 -0300

    Finalizando testes do Pedido

--- 02 - TDD/src/NerdStore.Vendas.Application/Coomands/PedidoCommandHandler.cs
+++ 02 - TDD/src/NerdStore.Vendas.Application/Coomands/PedidoCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using MediatR;
+using NerdStore.Core.DomainObjects;
+using NerdStore.Core.Messages;
 using NerdStore.Vendas.Application.Events;
 using NerdStore.Vendas.Domain;
 
@@ -17,17 +19,51 @@ namespace NerdStore.Vendas.Application.Coomands
 
         public async Task<bool> Handle(AdicionarItemPedidoCommand message, CancellationToken cancellationToken)
         {
+            if (!ValidarComando(message)) return false;
+
+            var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(message.ClienteId);
             var pedidoItem = new PedidoItem(message.ProdutoId, message.Nome, message.Quantidade, message.ValorUnitario);
-            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(message.ClienteId);
-            pedido.AdicionarItem(pedidoItem);
 
-            _pedidoRepository.Adicionar(pedido);
+            if (pedido == null)
+            {
+                pedido = Pedido.PedidoFactory.NovoPedidoRascunho(message.ClienteId);
+                pedido.AdicionarItem(pedidoItem);
 
-            pedido.AdicionarEvento(new PedidoItemAdicionadoEvent(pedido.ClienteId, pedido.Id, message.ProdutoId, message.Nome, message.ValorUnitario, message.Quantidade));
+                _pedidoRepository.Adicionar(pedido);
+            }
+            else
+            {
+                var pedidoItemExistente = pedido.PedidoItemExistente(pedidoItem);
+                pedido.AdicionarItem(pedidoItem);
 
-            await _pedidoRepository.UnitOfWork.Commit();
+                if (pedidoItemExistente)
+                {
+                    _pedidoRepository.AtualizarItem(pedido.PedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId));
+                }
+                else
+                {
+                    _pedidoRepository.AdicionarItem(pedidoItem);
+                }
 
-            return true;
+                _pedidoRepository.Atualizar(pedido);
+            }
+
+            pedido.AdicionarEvento(new PedidoItemAdicionadoEvent(pedido.ClienteId, pedido.Id, message.ProdutoId,
+                message.Nome, message.ValorUnitario, message.Quantidade));
+
+            return await _pedidoRepository.UnitOfWork.Commit();
+        }
+
+        private bool ValidarComando(Command message)
+        {
+            if (message.EhValido()) return true;
+
+            foreach (var error in message.ValidationResult.Errors)
+            {
+                _mediator.Publish(new DomainNotification(message.MessageType, error.ErrorMessage));
+            }
+
+            return false;
         }
     }
 }
--- 02 - TDD/src/NerdStore.Vendas.Domain/IPedidoRepository.cs
+++ 02 - TDD/src/NerdStore.Vendas.Domain/IPedidoRepository.cs
@@ -5,5 +5,9 @@ namespace NerdStore.Vendas.Domain
     public interface IPedidoRepository : IRepository<Pedido>
     {
         void Adicionar(Pedido pedido);
+        void Atualizar(Pedido pedido);
+        Task<Pedido> ObterPedidoRascunhoPorClienteId(Guid clienteId);
+        void AdicionarItem(PedidoItem pedidoItem);
+        void AtualizarItem(PedidoItem pedidoItem);
     }
 }
